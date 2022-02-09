7ac7b00b6b9ffd1f5f817dbea2b53cd61256ea7d
Jose Luis <joseyananicolas@gmail.com>
2022-02-09 01:37:53 -0500

    sockets

--- server.js
+++ server.js
@@ -1,52 +1,66 @@
 const express = require('express');
+const fs = require('fs');
+
+const {Server: HttpServer} = require('http');
+const {Server: IOServer} = require('socket.io');
 const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
-const fetch  = require('node-fetch');
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
-app.use(express.static('public'));
+let mensajes = [];
+const productos = []
+const ruta = './chat.txt'
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-/* ---------------------- hbs Motor ----------------------*/
-/*const exphbs = require('express-handlebars');
-app.set('views', path.join(__dirname, 'views/hbs'));
-app.engine('hbs', exphbs.engine({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), '/layouts'),
-    extname: 'hbs'
-}));
+async function leerArchivo(ruta) {
+    
+    try {
+        let resp =  await fs.promises.readFile(ruta, 'utf-8');
+        return resp === '' ? [] : JSON.parse(resp)
+    } catch (err) {
+        console.log(err)
+        return []
+    }
+}
 
-app.set('view engine', 'hbs');*/
-
-/* ---------------------- ejs Motor ----------------------*/
-
-/*app.set('views', './views/ejs');
-app.set('view engine', 'ejs');
-*/
-
-/* ---------------------- pug Motor ----------------------*/
-
-app.set('views', path.join(__dirname, 'views/pug'));
-app.set('view engine', 'pug');
+async function grabarArchivo(ruta,newData) {
+    try {
+        await fs.promises.writeFile(ruta, newData);
+    } catch (err) {
+        throw new Error('No grabo el archivo');
+    }
+}
 
 
-app.get('/', (req, res)=>{
-    res.render('home');
-});
-app.get('/productos',async (req, res)=>{
-    const response = await fetch('http://localhost:8080/api/productos');
-    const productos = await response.json();
-    res.render('list',{productos});
-});
 
 const routerProductos = require('./api/route');
 app.use('/api/productos', routerProductos);
 
+
+io.on('connection', async (socket) => {
+    console.log(`Nuevo cliente conectado ${socket.id}`);
+
+    socket.emit('listProduct', productos);
+    socket.on('newProductos',  (data)=>{
+        productos.push(data);
+        io.sockets.emit('listProduct', productos);
+    });
+
+    mensajes = await leerArchivo(ruta)
+    socket.emit('mensajes', mensajes);
+    socket.on('mensajeNuevo', async (data)=>{
+        mensajes.push(data);
+        await grabarArchivo(ruta,JSON.stringify(mensajes))
+        io.sockets.emit('mensajes', mensajes);
+    });
+});
 const PORT = 8080;
-const server = app.listen(PORT, ()=>{
+const server = httpServer.listen(PORT, ()=>{
     console.log(`Servidor escuchando en puerto ${PORT}`);
 })
 server.on('error', error=>{
